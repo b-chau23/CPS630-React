0d14eb38e02c653ec8f66a2e1c6e3033c8e069dd
b-chau23 <bowiechau2301@gmail.com>
2025-03-24 17:02:52 -0400

    Navbar and Layout

---
+++ cps630-app/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+import { useAuthContext } from "../authContext";
+
+import '../styles/navbar.css';
+
+function Navbar() {
+    const auth = useAuthContext();
+    
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/about'>About</NavLink></li>
+                    {auth.username ? <li><NavLink to='/Logout'>Log Out</NavLink></li> :
+                    <>
+                        <li><NavLink to='/SignIn'>Sign In</NavLink></li>
+                        <li><NavLink to='/SignUp'>Sign Up</NavLink></li>
+                    </>
+                    }
+                    {auth.role === 'admin' && <li><NavLink to='/DbMaintain'>DB Maintain</NavLink></li>}
+                    
+                    
+                    <NavLink to='/'></NavLink>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+
+export default Navbar;

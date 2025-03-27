69492a543a5f50fce2844c2d56709bf5534be106
b-chau23 <bowiechau2301@gmail.com>
2025-03-27 02:22:09 -0400

    logout bug fix

--- cps630-app/src/pages/Logout.tsx
+++ cps630-app/src/pages/Logout.tsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "../authContext";
 
 function Logout() {
     const [errorMsg, setErrorMsg] = useState('');
     const auth = useAuthContext();
     
-    fetch('http://localhost/proj2/php/logout.php', {
-        method: 'DELETE',
-        credentials: 'include'
-    })
-    .catch(error => setErrorMsg(error));
-    auth.setRole('');
-    auth.setUsername('')
+    useEffect(() => {
+        fetch('http://localhost/proj2/php/logout.php', {
+            method: 'DELETE',
+            credentials: 'include'
+        })
+        .catch(error => setErrorMsg(error));
+        auth.setRole('');
+        auth.setUsername('')
+    }, [])
     
     if (errorMsg) {
         return <><h1>Something went Wrong :/</h1><p>{errorMsg}</p></>
